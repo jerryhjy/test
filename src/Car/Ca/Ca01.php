8a49248cbfd4697bafe46f1d4ca63d3a4a084550
jerry_he <hejunyi1204@163.com>
2016-03-26 16:56:18 +0800

    test

---
+++ src/Car/Ca/Ca01.php
@@ -0,0 +1,13 @@
+<?php
+  
+namespace Car\Aa;
+  
+class Aa01
+{
+    public static function info()
+    {
+        echo "This is Car Aa2013!<br />";
+    }
+}
+
+
---
+++ src/Car/Da/Da01.php
@@ -0,0 +1,13 @@
+<?php
+  
+namespace Car\Da;
+  
+class Da01
+{
+    public static function info()
+    {
+        echo "This is Car Da2013!<br />";
+    }
+}
+
+
